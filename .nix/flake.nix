0698fa6f3d139175b65ab18586f28d4d9b0e105d
Devon Richards <friends.devon@gmail.com>
2023-02-27 13:26:19 -0600

    bunch of updates.

---
+++ .nix/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "CubeArtisan Infrastructure";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        name = "cubeartisan-infrastructure";
+        buildInputs = [
+          pkgs.kubectl
+          pkgs.kustomize
+          pkgs.velero
+        ];
+      };
+    }
+  );
+}
